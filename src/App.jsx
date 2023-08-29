2b87264ec1e377f50e2fb9dcb1a618b0b68848c5
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-08-29 18:21:57 +0200

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,7 @@ import { useState } from "react";
 export default function App() {
   const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([])
-  function handleSubmit (e) {
-    e.preventDefault()
-
-    setTodos( (currentTodos) => {
-        return [
-          [...todos, 
-            {id:crypto.randomUUID(), title: newItem, completed:false},
-        ]
-    ]})
-      
-  }
-  setNewItem("")
+ 
  
 function toggleTodo(id, completed) {
   setTodos(currentTodos => {
@@ -28,19 +17,18 @@ function toggleTodo(id, completed) {
   })
 
 }
+function deleteTodo ()  {
+  setTodos(currentTodos => {
+    return currentTodos.filter(todod => todo.id != id)
+  })
+}
 
   return (
     <>
-  <form onSubmit={handleSubmit} className="new-item-form">
-    <div className="row">
-      <label htmlFor="item"> New item</label>
-      <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" />
-    </div>
   
-    <button className="btn">Add</button>
-    </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
+      {todo.length == 0 && "No Todos"}
       {todos.map(todo => {
         return (
         <li key={todo.id}>
@@ -48,7 +36,7 @@ function toggleTodo(id, completed) {
           <input type="checkbox" checked={todo.completed} onChange={e =>toggleTodo(todo.id, e.target.checked) } />
           {todo.title}
         </label>
-        <button className="btn.btn-danger">remove</button>
+        <button onClick={() => deleteTodo(todo.id)}  className="btn.btn-danger">remove</button>
       </li>
       )
       })}
